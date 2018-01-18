51c5008e6c408b4acd3f7f79e5d7a3766daddf6a
毕殿卿 <bdq123@outlook.com>
2018-01-19 00:09:21 +0800

    *

--- AspNetCoreUseTinyMCE/Controllers/HomeController.cs
+++ AspNetCoreUseTinyMCE/Controllers/HomeController.cs
@@ -5,11 +5,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreUseTinyMCE.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace AspNetCoreUseTinyMCE.Controllers
 {
     public class HomeController : Controller
     {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
         public IActionResult Index()
         {
             return View();
@@ -32,6 +41,17 @@ namespace AspNetCoreUseTinyMCE.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var uploads = Path.Combine(_environment.WebRootPath, "uploads");
+            using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+            return Json(new { location = $"/uploads/{file.FileName}" });
         }
     }
 }
